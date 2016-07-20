8ad0eff50bd9efa28e1cbf947b367415c5e11d4f
Carol Ng <ng.km.carol@gmail.com>
2016-07-20 15:02:52 -0700

    Rename LRS constants, add IR detection to handleIntersect

--- tinahmain/constants.h
+++ tinahmain/constants.h
@@ -23,21 +23,22 @@
 #define GRIPSERVO 1
 #define IRSERVO 2
 
-#define IRINTFRONT 0
-#define IRINTLEFT 1
-#define IRINTRIGHT 2
+#define IRFRONT 0
+#define IRFRONTLEFT 1
+#define IRFRONTRIGHT 2
 #define IRLEFT 3
 #define IRRIGHT 4
-#define IRGROUP 5
+#define IRGRIP 5
 #define POSARM1 6
 #define POSARM2 7
 
 #define KNOB1 6
 #define KNOB2 7
 
+// corresponds to map indices
+#define LEFT 0
 #define RIGHT 1
-#define STRAIGHT 0
-#define LEFT -1
+#define STRAIGHT 2
 
 #define TRUE 1
 #define FALSE 0
---
+++ tinahmain/handleIntersect2.ino
@@ -0,0 +1,47 @@
+void handleIntersect2() {
+  int temp;
+  int lInt = !digitalRead(LINTTAPE);
+  int rInt = !digitalRead(RINTTAPE);
+
+  boolean front = (nodeMap[lastNode][nextNode][STRAIGHT] != -1);
+  boolean left = (nodeMap[lastNode][nextNode][LEFT] != -1);
+  boolean right = (nodeMap[lastNode][nextNode][RIGHT] != -1);
+
+  int intersect = scanIntersection(right, left, front);
+
+  if(intersect == LEFT) {
+    hardLeft();
+    temp = nodeMap[lastNode][nextNode][0];
+    lastNode = nextNode;
+    nextNode = temp;
+  }
+  else if(intersect == RIGHT) {
+    hardRight();
+    temp = nodeMap[lastNode][nextNode][1];
+    lastNode = nextNode;
+    nextNode = temp;
+  }
+  else{
+    temp = nodeMap[lastNode][nextNode][2];
+    lastNode = nextNode;
+    nextNode = temp;
+  }
+
+// scan intersection with IR detectors to choose the one with highest intensity
+int scanIntersection(boolean right, boolean left, boolean front){
+  int lIntIr = -1;
+  int rIntIr = -1;
+  int fIntIr = -1;
+
+  if(left){
+    lIntIr = analogRead(IRFRONTLEFT);
+  }
+  if(right){
+    rIntIr  = analogRead(IRFRONTRIGHT);
+  }
+  if(front){
+    fIntIr = analogRead(IRFRONT);
+  }
+  int Irs[3] = {lIntIr, rIntIr, fIntIr};
+  return findMax(Irs, 3);
+}
