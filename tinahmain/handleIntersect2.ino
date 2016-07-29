18e9908e674c10eeec6309e5bea3f1902ef11c45
Carol Ng <ng.km.carol@gmail.com>
2016-07-28 21:18:51 -0700

    Refactor turn decision methods

--- tinahmain/handleIntersect2.ino
+++ tinahmain/handleIntersect2.ino
@@ -7,15 +7,13 @@ void handleIntersect2() {
   boolean left = (nodeMap[lastNode][nextNode][LEFT] != -1);
   boolean right = (nodeMap[lastNode][nextNode][RIGHT] != -1);
 
-  int intersect = scanIntersection(right, left, front);
-
-  if(intersect == LEFT) {
+  if(turnDecision == LEFT) {
     hardLeft();
     temp = nodeMap[lastNode][nextNode][0];
     lastNode = nextNode;
     nextNode = temp;
   }
-  else if(intersect == RIGHT) {
+  else if(turnDecision == RIGHT) {
     hardRight();
     temp = nodeMap[lastNode][nextNode][1];
     lastNode = nextNode;
@@ -26,23 +24,4 @@ void handleIntersect2() {
     lastNode = nextNode;
     nextNode = temp;
   }
-}
-
-// scan intersection with IR detectors to choose the one with highest intensity
-int scanIntersection(boolean right, boolean left, boolean front){
-  int lIntIr = -1;
-  int rIntIr = -1;
-  int fIntIr = -1;
-
-  if(left){
-    lIntIr = analogRead(IRFRONTLEFT);
-  }
-  if(right){
-    rIntIr  = analogRead(IRFRONTRIGHT);
-  }
-  if(front){
-    fIntIr = analogRead(IRFRONT);
-  }
-  int Irs[3] = {lIntIr, rIntIr, fIntIr};
-  return findMax(Irs, 3);
 }
