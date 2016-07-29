12f45cd4c09a818940e045594b3a2ae42aeba2ec
Carol Ng <ng.km.carol@gmail.com>
2016-07-29 11:26:59 -0700

    Add grip servo test, variable motor speed

--- tinahmain/armTest.ino
+++ tinahmain/armTest.ino
@@ -1,26 +1,33 @@
 void armTest() {
-  while(TRUE){
+  int motorSpeed;
+  int negMotorSpeed;
+  while(!(startbutton() && stopbutton())){
     while (!startbutton()){
       if (!(c%200)) {
         LCD.clear();
         LCD.setCursor(0,0);
-        LCD.print("Knob to 10: ");
-        LCD.print(knob(6));
+        LCD.print("Motor speed: ");
+        LCD.print(knob(6)/2);
         LCD.setCursor(0,1);
         LCD.print("START to cont.");   
       }
     }
+    motorSpeed = knob(6)/2;
+    negMotorSpeed = -1*motorSpeed;
     while(!stopbutton()){
       if (!(c%200)) {
         LCD.clear();
         LCD.setCursor(0,0);
-        LCD.print("Arm Servo: ");
+        LCD.print("Op/cl servo: ");
         LCD.print(knob(6));
         LCD.setCursor(0,1);
         LCD.print("STOP to cont.");   
-        
+        if (knob(6) > 500) {
+          RCServo0.write(GRIPSERVMAX);
+        } else {
+          RCServo0.write(GRIPSERVMIN);
+        }
       }
-      RCServo0.write(knob(6));
     }
     while (!startbutton()){
       if (!(c%200)) {
@@ -29,7 +36,7 @@ void armTest() {
         LCD.print("+ armspeed:");
         LCD.setCursor(0,1);
         LCD.print("START to cont.");   
-        motor.speed(2, 100);
+        motor.speed(2, motorSpeed);
         
       }
     }
@@ -40,8 +47,7 @@ void armTest() {
         LCD.print("- armspeed:");
         LCD.setCursor(0,1);
         LCD.print("STOP to cont.");   
-        motor.speed(2, -100);
-        
+        motor.speed(2, negMotorSpeed);
       }
     }
     motor.stop_all();
