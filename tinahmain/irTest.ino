494fe8ac5106d3e2f29e1b4ef562e5c989fa8bf8
Carol Ng <ng.km.carol@gmail.com>
2016-07-29 09:59:04 -0700

    Add new print method with fewer spaces

--- tinahmain/irTest.ino
+++ tinahmain/irTest.ino
@@ -2,11 +2,9 @@ void IRTest() {
   while (!stopbutton()) {
     LCD.clear();
     LCD.setCursor(0,0);
-    LCD.print("L:");
-    labelPrint("L", analogRead(IRFRONTLEFT),"R", analogRead(IRFRONTRIGHT),"S", analogRead(IRFRONT));
+    irPrint("L", analogRead(IRFRONTLEFT),"R", analogRead(IRFRONTRIGHT),"S", analogRead(IRFRONT));
     LCD.setCursor(0,1);
-    LCD.print("PU ");
-    labelPrint("L", analogRead(IRLEFT),"R", analogRead(IRRIGHT),"",0);
+    irPrint("L", analogRead(IRLEFT),"R", analogRead(IRRIGHT),"",0);
   }
   LCD.clear();
   LCD.setCursor(0,0);
