3e6cfc48b3bf0a9d4c399225d042204178882241
Carol Ng <ng.km.carol@gmail.com>
2016-08-03 23:12:09 -0700

    FIXED THE MAP

--- tinahmain/findMax.ino
+++ tinahmain/findMax.ino
@@ -1,12 +1,17 @@
 int findMax(int intArray[], int arraySize){
   int i, val, id;
-  val = 0;
+  val = -2;
+  Serial.print("\nfindMax - intArray:");
   for (i=0;i<arraySize;i++) {
     if (intArray[i] > val) {
       id = i;
       val = intArray[i];
     }
+    Serial.print(intArray[i]);
+    Serial.print(" ");
   }
+  Serial.print("\nfindMax - returned:");
+  Serial.print(id);
   return id;
 }
 
--- tinahmain/handleIntersect2.ino
+++ tinahmain/handleIntersect2.ino
@@ -2,19 +2,13 @@ void handleIntersect2() {
   int temp;
   int lInt = !digitalRead(LINTTAPE);
   int rInt = !digitalRead(RINTTAPE);
-
-  // Nodes 0-4 are the top nodes; if the tape follower turns around before reaching them instead of bumping into the side, reverse the nodes
-  // Nodes 5 and 9 are the "dead end" nodes. The tape follower will reach the end of the tape and automatically turn around.
-  if ((nextNode <= 5 && nextNode >= 0) || nextNode == 9) {
-    temp = lastNode;
-    lastNode = nextNode;
-    nextNode = temp;
-  }
+  
   boolean front = (nodeMap[lastNode][nextNode][STRAIGHT] != -1);
   boolean left = (nodeMap[lastNode][nextNode][LEFT] != -1);
   boolean right = (nodeMap[lastNode][nextNode][RIGHT] != -1);
 
   motor.stop_all();
+
   while (! startbutton()) {
     LCD.clear();
     LCD.setCursor(0,0);
@@ -35,7 +29,17 @@ void handleIntersect2() {
     LCD.print("START to continue");
   }
   while(startbutton()) {
+    LCD.print("CHILLING");
     // just chill
+  }
+
+  // Nodes 0-4 are the top nodes; if the tape follower turns around before reaching them instead of bumping into the side, reverse the nodes
+  // Nodes 5 and 9 are the "dead end" nodes. The tape follower will reach the end of the tape and automatically turn around.
+  if ((nextNode <= 5 && nextNode >= 0) || nextNode == 9) {
+    temp = lastNode;
+    lastNode = nextNode;
+    nextNode = temp;
+    return;
   }
   
   if(turnDecision == LEFT) {
